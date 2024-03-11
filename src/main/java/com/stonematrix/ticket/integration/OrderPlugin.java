05245dad55c04fb8d0274ae0977749f3a809c7a2
grantsunny <grantsunny@msn.com>
2024-03-11 21:06:06 +0800

    First implementation of the plug-in - also added a switch to force x-ticket-userId cookie.

--- src/main/java/com/stonematrix/ticket/integration/OrderPlugin.java
+++ src/main/java/com/stonematrix/ticket/integration/OrderPlugin.java
@@ -1,6 +1,7 @@
 package com.stonematrix.ticket.integration;
 
 import com.stonematrix.ticket.api.model.Order;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
@@ -8,6 +9,31 @@ import java.util.Map;
  * In order to enable a plugin, simply implement this interface and register it to Spring by @Component.
  */
 public interface OrderPlugin {
+
+    @Component
+    public static class DummyOrderPlugin implements OrderPlugin {
+        @Override
+        public boolean matches(String eventId, String userId, Map<String, Object> eventMetadata) throws OrderPluginException {
+            return false;
+        }
+
+        @Override
+        public Order beforePlaceOrder(Order order) throws OrderPluginException {
+            return null;
+        }
+
+        @Override
+        public Order beforePayOrder(Order order, Integer payAmount) throws OrderPluginException {
+            return null;
+        }
+
+        @Override
+        public void beforeCancelOrder(Order order) throws OrderPluginException {
+
+        }
+    }
+
+
 
     boolean matches(String eventId, String userId, Map<String, Object> eventMetadata) throws OrderPluginException;
 
