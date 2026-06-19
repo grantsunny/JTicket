17f2dee9456a5e9e0b18bcfe25aefbf2581463b9
Grant Yang <grantsunny@msn.com>
2026-06-19 12:01:06 +0800

    Fix PR authorization and build repeatability

--- src/main/java/com/jticket/endpoints/EndpointsConfig.java
+++ src/main/java/com/jticket/endpoints/EndpointsConfig.java
@@ -3,15 +3,19 @@ package com.jticket.endpoints;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EndpointsConfig extends ResourceConfig {
 
-    public EndpointsConfig() {
+    public EndpointsConfig(Environment environment) {
         property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, "true");
+        if (environment.acceptsProfiles(Profiles.of("production"))) {
+            register(RolesAllowedDynamicFeature.class);
+        }
         registerClasses(
-                RolesAllowedDynamicFeature.class,
                 UserTokenFilter.class,
                 DateObjectMapperProvider.class,
                 ApiDocResource.class,
